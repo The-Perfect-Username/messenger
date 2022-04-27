b6ee49bd8102e0757cc835b20fdf1e19b3cba041
Lachlan Enderlin <lendo1995@gmail.com>
2022-04-27 20:36:51 +0930

    Created basic server client message system

---
+++ client.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    
+    while True:
+        user_message = input("Type: ")
+
+        if len(user_message) > 0:
+            s.sendall(bytes(user_message, encoding="utf-8"))
+        else:
+            break
+
+        data = s.recv(1024)
+        print(str(data, encoding="utf-8"))
