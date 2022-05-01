ca57f7f8478be41a0150bb03b6d0b6d7e16a60e8
Lachlan Enderlin <lendo1995@gmail.com>
2022-05-01 14:23:51 +0930

    Multi-connection server

--- server.py
+++ server.py
@@ -1,24 +1,57 @@
+from select import select
+import sys
 import socket
+import selectors
+import types
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+selector = selectors.DefaultSelector()
+
+HOST, PORT = sys.argv[1], int(sys.argv[2])
+
 MAX_SIZE = 1024
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    selector.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            selector.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as lsock:
+    lsock.bind((HOST, PORT))
+    lsock.listen()
+    lsock.setblocking(False)
     
-    conn, addr = s.accept()
-    
-    with conn:
-        print(f"Connected by {addr}")
+    selector.register(lsock, selectors.EVENT_READ, data=None)
+
+    try:
         while True:
-            data = conn.recv(MAX_SIZE)
-            
-            if data: 
-                print("Client:", str(data, encoding="utf-8"))
-            else:
-                break
-            
-            conn.sendall(data)
-        print ("Closed session with client")
+            events = selector.select(timeout=None)
+            for key, mask in events:
+                if key.data is None:
+                    accept_wrapper(key.fileobj)
+                else:
+                    service_connection(key, mask)
+    except KeyboardInterrupt:
+        print("Caught keyboard interrupt, exiting")
+    finally:
+        selector.close()
