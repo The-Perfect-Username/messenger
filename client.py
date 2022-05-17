c038804bf05ee5fd2ab66dd61124295022c643e0
Lachlan Enderlin <lendo1995@gmail.com>
2022-05-17 17:27:37 +0930

    Attempt at fixing EOFError

--- client.py
+++ client.py
@@ -25,15 +25,14 @@ class Client:
             
             self.lthread.acquire()
             
-            while True:
-                self.t1 = threading.Thread(target=self.handle_recieved_data)
-                self.t2 = threading.Thread(target=self.handle_user_input)
+            self.t1 = threading.Thread(target=self.handle_recieved_data)
+            self.t2 = threading.Thread(target=self.handle_user_input)
 
-                self.t1.start()
-                self.t2.start()
+            self.t1.start()
+            self.t2.start()
 
-                self.t1.join()
-                self.t2.join()
+            self.t1.join()
+            self.t2.join()
 
         except KeyboardInterrupt:
             if self.t1 and self.t2:
@@ -51,22 +50,28 @@ class Client:
 
     def handle_user_input(self):
         try:
-            user_message = input("Type message: ")
-            
-            if (user_message):
-                self.sock.send(bytes(user_message, encoding="utf-8"))
+            while True:
+                user_message = input("Type message: ")
+                
+                if (user_message):
+                    self.sock.send(bytes(user_message, encoding="utf-8"))
         except ConnectionAbortedError:
             print ("Connection was aborted handle user")
+        except EOFError:
+            # This will occur no matter what when exiting the terminal while method is used in thread
+            print ("EOFError occurred while exiting")
+            self.stop = True
 
     def handle_recieved_data(self):
         try:
-            response = self.sock.recv(MAX_SIZE)
+            while True:
+                response = self.sock.recv(MAX_SIZE)
 
-            if not response:
-                self.lthread.release()
+                if not response:
+                    self.lthread.release()
 
-            if len(response):
-                print (response.decode("utf-8"))
+                if len(response):
+                    print (response.decode("utf-8"))
 
         except ConnectionAbortedError as e:
             print ("Connection was aborted handle response")
